84524ed3675ee779dcc72d14e8fa1cbecbb97ab7
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-08-02 00:46:40 +0900

    ✨ eslint の設定変更

--- src/styles/themes/theme.ts
+++ src/styles/themes/theme.ts
@@ -2,20 +2,20 @@ import { createTheme } from '@mui/material/styles'
 
 export const defaultTheme = createTheme({
   palette: {
-    primary: {
-      light: '#3581FF',
-      main: '#3C44FF',
-      dark: '#2329A7',
-      contrastText: '#FFFFFF',
-    },
-    secondary: {
-      light: '#FF7570',
-      main: '#FF5C57',
-      dark: '#B32F2B',
-      contrastText: '#FFFFFF',
-    },
     background: {
       default: '#F7FAFF',
+    },
+    primary: {
+      contrastText: '#FFFFFF',
+      dark: '#2329A7',
+      light: '#3581FF',
+      main: '#3C44FF',
+    },
+    secondary: {
+      contrastText: '#FFFFFF',
+      dark: '#B32F2B',
+      light: '#FF7570',
+      main: '#FF5C57',
     },
     text: {
       primary: '#555555',
