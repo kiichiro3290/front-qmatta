081fdf4b13d2405b192af259c4e462b42f657965
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-09-11 01:08:27 +0900

    ヘッダーにアバターアイコンを追加

--- src/components/layouts/HeaderLayout/HeaderLayout.tsx
+++ src/components/layouts/HeaderLayout/HeaderLayout.tsx
@@ -1,5 +1,5 @@
 import { AccountCircle, Menu } from '@mui/icons-material'
-import { AppBar, Box, Drawer, IconButton, MenuItem, Toolbar, Typography } from '@mui/material'
+import { AppBar, Avatar, Box, Drawer, IconButton, MenuItem, Toolbar, Typography } from '@mui/material'
 import { useCallback, useState } from 'react'
 import { SideBar } from '../SideBar/SideBar'
 
@@ -16,17 +16,25 @@ export const HeaderLayout = () => {
   return (
     <AppBar position='fixed'>
       <Toolbar>
-        <Box>
-          <IconButton size='large' edge='start' color='inherit' aria-label='menu' onClick={onClickMenuButton}>
-            <Menu />
-          </IconButton>
-          <Drawer anchor='left' open={isShowSideBar} onClose={onCloseMenuButton}>
-            <Box sx={{ px: '16px' }}>
-              <SideBar />
-            </Box>
-          </Drawer>
+        <Box
+          display='flex'
+          width='100%'
+          flexDirection='row'
+          sx={{ alignItems: 'center', justifyContent: 'space-between' }}
+        >
+          <Box display='flex' sx={{ alignItems: 'center' }}>
+            <IconButton size='large' edge='start' color='inherit' aria-label='menu' onClick={onClickMenuButton}>
+              <Menu />
+            </IconButton>
+            <Drawer anchor='left' open={isShowSideBar} onClose={onCloseMenuButton}>
+              <Box sx={{ px: '16px' }}>
+                <SideBar />
+              </Box>
+            </Drawer>
+            <Typography>Qmatta</Typography>
+          </Box>
+          <Avatar />
         </Box>
-        <Typography>Qmatta</Typography>
       </Toolbar>
     </AppBar>
   )
