adda7f05b5fb34451f5a1d7616623c5447f61c66
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-09-28 11:04:34 +0900

    addjust dialog width

--- src/components/layouts/QmaDialogue/QmaDialogue.tsx
+++ src/components/layouts/QmaDialogue/QmaDialogue.tsx
@@ -7,7 +7,7 @@ export type QmaDialogueProps = {
 
 export const QmaDialogue: React.FC<QmaDialogueProps> = ({ dialogues }) => {
   return (
-    <Box sx={{ px: '20px', width: 400 }}>
+    <Box sx={{ px: '20px', width: '400px' }}>
       {dialogues.map((dialogue, index) => (
         <Box key={index} sx={{ backgroundColor: 'grey.100', borderRadius: '16px', my: '20px', p: '12px 24px' }}>
           <Typography sx={{ fontSize: '16px' }}>{dialogue}</Typography>
