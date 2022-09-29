32892fc43cf04a8feebebb5c41aea1b00020c153
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-09-29 10:35:09 +0900

    define type of bear function

--- src/pages/api/bear.ts
+++ src/pages/api/bear.ts
@@ -5,13 +5,17 @@ import axios from 'axios'
 
 const url = process.env.NEXT_PUBLIC_BASE_URL ?? ''
 
+// クマのメッセージを取得する（簡易版）: GET
+// () => message: string
 export const fetchQmaMessage = async () => {
   const data = await axios.get(url).then((res) => res.data)
   return data
 }
 
+// クマのメッセージを取得する & メッセージをDBに保存: POST
+// (userID: string, message: string) => message: string
 export const postQmaMessage = async (userId: string, message: string): Promise<string> => {
-  const data = { message: message }
+  const data: UserMessageToBear = { message }
   const result = await axios.post(`${url}/bear/${userId}`, data)
   return result.data.response
 }
--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,10 +1,11 @@
+import axios from 'axios'
+
+const url = process.env.NEXT_PUBLIC_BASE_URL ?? ''
+
 // スタンプを送信する: PATCH
-
-import axios from "axios"
-
-// (stampId) => {}
-export const sendStampStatus = async (stampId: string) => {
-        const result = await axios.patch(`${url}/bear/${userId}`, data)
-        return result.data.response
-      }
+// (stampId: string?, userId: string) => { 返り値が謎 }
+export const sendUserStatus = async (stampId: string, userId: string) => {
+  const data = { stampId }
+  const result = await axios.patch(`${url}/user/status/${userId}`, data)
+  return result.data.response
 }
