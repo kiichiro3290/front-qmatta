210df6949f08802ef9a0cb78ee700e834af70595
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-10-08 04:04:07 +0900

    save auth info into local storage

--- src/components/pages/login/index.tsx
+++ src/components/pages/login/index.tsx
@@ -1,10 +1,14 @@
 import { useRouter } from 'next/router'
 import { useCallback, useState } from 'react'
+import { useDispatch } from 'react-redux'
 import { logInUser } from '~/pages/api/user'
+import { AppDispatch } from '~/store'
+import { fetchUserDataState } from '~/store/user/actions'
 import { LogInPagePresenter } from './presenter'
 
 export const LogInPage: React.FC = () => {
   const router = useRouter()
+  const dispatch: AppDispatch = useDispatch()
 
   const [isOpenSnackbar, setIsOpenSnackbar] = useState<boolean>(false)
   const [snackbarMessage, setSnackbarMessage] = useState<string>('')
@@ -14,6 +18,9 @@ export const LogInPage: React.FC = () => {
     // メールアドレスとパスワードを認証する
     const result = await logInUser(email, password)
     if (result.result) {
+      localStorage.setItem('email', email)
+      localStorage.setItem('password', password)
+      dispatch(fetchUserDataState(result.user))
       router.push('/')
       return
     } else {
