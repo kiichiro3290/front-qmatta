ee3cd5d2d0348059d0ba87154e5e297b55383034
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-10-08 03:12:03 +0900

    difine type of users

---
+++ src/types/user.d.ts
@@ -0,0 +1,24 @@
+type UserRole = {
+  permission: number
+  rolename: string
+}
+type UserStatus = 'スッキリ' | '沼った'
+
+type UserData = {
+  bearIcon: string
+  bearTone: string
+  communityId: string[]
+  createdAt: Date
+  deletedAt: Date
+  emailAddress: string
+  icom: string
+  like: string[]
+  password: string
+  profile: string
+  question: string[]
+  role: UserRole
+  status: UserStatus
+  updatedAt: string
+  userName: string
+  _id: string
+}
