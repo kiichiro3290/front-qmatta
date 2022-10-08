2059171d0b7606f613846f965a38ada56f7041b6
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-10-08 11:16:55 +0900

    fix api

--- src/pages/api/bear.ts
+++ src/pages/api/bear.ts
@@ -9,16 +9,19 @@ const mebo_url = process.env.NEXT_PUBLIC_MEBO_URL ?? ''
 // クマのメッセージを取得する（簡易版）: GET
 // () => message: string
 export const fetchQmaMessage = async () => {
-  const data = await axios.get(`${url}/bear`).then((res) => res.data)
-  return data.response
+  const data = await axios.get(`${url}/bear`).then((res) => res.data.response)
+  return data
 }
 
 // クマのメッセージを取得する & メッセージをDBに保存: POST
 // (userID: string, message: string) => message: string
 export const postQmaMessage = async (userId: string, message: string): Promise<string> => {
   const data: UserMessageToBear = { message }
-  const result = await axios.post(`${url}/bear/${userId}`, data)
-  return result.data.response
+  const result = await axios
+    .post(`${url}/bear/${userId}`, data)
+    .then((res) => res.data.response)
+    .catch((e) => console.log(e))
+  return result
 }
 // meboのapiを叩いて，返答を取得する
 // 無料版で800messages/month, ¥2800/monthで10000messages/month
