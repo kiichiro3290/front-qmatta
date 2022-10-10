3aed2a84e2656a4cde9a84799adcf3687cd22de3
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-10-10 22:49:48 +0900

    fix api referense

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -1,26 +1,24 @@
-import axios from 'axios'
-
-const url = process.env.NEXT_PUBLIC_BASE_URL ?? ''
+import { qmattaClient } from './client'
 
 // スタンプを送信する: PATCH
 // (stampId: string?, userId: string) => { 返り値が謎 }
 export const sendUserStatus = async (stampId: string, userId: string) => {
   const data = { stampId }
-  const result = await axios.patch(`${url}/user/status/${userId}`, data)
+  const result = await qmattaClient().patch(`user/status/${userId}`, data)
   return result.data.response
 }
 
 // メッセージの送信履歴を取得する: GET
 // (userId: string) => string[]
 export const getMessageHistory = async (userId: string): Promise<string[]> => {
-  const result = await axios.get(`${url}/bear/history/${userId}`)
+  const result = await qmattaClient().get(`bear/history/${userId}`)
   return result.data.message
 }
 
 // 自分が加入しているコミュニティの一覧を取得する: GET
 // (userId: string) => comunity_name: string[]
 export const getCommunityList = async (userId: string): Promise<string[]> => {
-  const result = await axios.get(`${url}/user/community/${userId}`)
+  const result = await qmattaClient().get(`user/community/${userId}`)
   const communityList = result.data.communityName
   return communityList
 }
@@ -32,7 +30,7 @@ export const signUpUser = async (emailAddress: string, password: string) => {
     emailAddress,
     password,
   }
-  const response = await axios.post(`${url}/signup`, body)
+  const response = await qmattaClient().post('signup', body)
   const data = response.data
   return { msg: data.msg, result: data.result }
 }
@@ -44,7 +42,7 @@ export const logInUser = async (emailAddress: string, password: string) => {
     emailAddress,
     password,
   }
-  const response = await axios.post(`${url}/login`, body)
+  const response = await qmattaClient().post('login', body)
   const data = response.data
   return { result: data.result, user: data.user }
 }
