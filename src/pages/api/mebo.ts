68774c770a39b71e43f5c1219d8a709e6185cb25
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-11-12 01:51:52 +0900

    refactor if else to switch case

--- src/pages/api/mebo.ts
+++ src/pages/api/mebo.ts
@@ -2,23 +2,37 @@ import { getAIQmaMessage } from '~/api/backend/mebo'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  answer?: string
+type Error = {
+  errorCode: number
+  errorMessage: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  if (req.method === 'GET') {
-    const userId = req.query.userId as string
-    const dialogue = req.query.dialogue as string
+type ResponseData = {
+  answer?: string
+  error?: Error
+}
 
-    ;(async () => {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  switch (req.method) {
+    case 'GET': {
+      // string[]の時がある?
+      const userId = req.query.userId as string
+      const dialogue = req.query.dialogue as string
+
+      // このエラー処理する必要がある？
       const answer = await getAIQmaMessage(userId, dialogue)
       return res.status(200).json({ answer })
-    })()
-  } else {
-    return res.status(200).json({})
+    }
+    default: {
+      return res.status(405).json({
+        error: {
+          errorCode: 405,
+          errorMessage: '405 Method Not Allowed',
+        },
+      })
+    }
   }
 }
