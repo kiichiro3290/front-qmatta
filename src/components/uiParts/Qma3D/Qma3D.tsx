1b77288f30b93c9016c21718cc7110a367f76848
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-12-18 02:04:10 +0900

    update model shadow

--- src/components/uiParts/Qma3D/Qma3D.tsx
+++ src/components/uiParts/Qma3D/Qma3D.tsx
@@ -29,11 +29,20 @@ export const Qma3D: React.FC = () => {
   // TODO: cameraの位置
   // TODO: shadower
   // TODO: canvasの大きさのレスポンシブ
+  // x:0 横向きに回転する, y:0 ？, z: 縦向きに回転する
+
+  // ambientLight 環境光源：3D空間全体を照らす役割，これ単体で使うことは少なく，大抵の場合他の高原と一緒に利用する
+  // パラメータ：intensityを高くすると明るくなる
+  // directionLight 平行光源
+  // パラメータ：position: x横から，y上から，z前から
+  // spotLight : スポットライト
+  // パラメータ：色, 光の強さ, 距離, 照射角, ボケ具合, 減衰率
   return (
-    <Canvas camera={{ position: [100, 100, 800], fov: 50 }} shadows>
+    <Canvas camera={{ position: [0, 200, 900], fov: 50 }}>
       <Suspense fallback={null}>
-        <ambientLight intensity={2} />
-        <directionalLight color='white' position={[0, 0, 5]} />
+        <ambientLight intensity={4} />
+        <directionalLight color='white' intensity={4} position={[0, 2, 1]} />
+        <spotLight color='white' intensity={4} position={[0, 4, -1]} />
         <Scene />
         <OrbitControls />
       </Suspense>
