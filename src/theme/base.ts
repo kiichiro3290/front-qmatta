e7348774e5e65bd5847bbd9428db33920c1cdda5
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-12-24 21:42:26 +0900

    add comment

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -1,5 +1,11 @@
 import { createTheme } from '@mui/material/styles'
 
+/**
+ * スクロールバーのカスタマイズ参考
+ * https://unformedbuilding.com/articles/learn-about-webkit-scrollbar/
+ * https://www.ultra-noob.com/blog/2021/58/
+ */
+
 export const baseTheme = createTheme({
   shape: { borderRadius: 3 },
   typography: {
