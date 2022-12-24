5a5abebad75d70c33f0b6b4d4637e438783741e8
kiichiro3290 <kai.kiichiro.418@s.kyushu-u.ac.jp>
2022-12-24 21:41:33 +0900

    customize scroll bar in chat balloon

--- src/theme/base.ts
+++ src/theme/base.ts
@@ -9,4 +9,22 @@ export const baseTheme = createTheme({
     fontWeightMedium: 500,
     fontWeightRegular: 400,
   },
+
+  components: {
+    //`MuiCssBaseline`になっているが`CssBaseLine`ても同様に作用した
+    MuiCssBaseline: {
+      styleOverrides: `
+        ::-webkit-scrollbar{
+            width: 10px;
+        },
+        ::-webkit-scrollbar-thumb {
+            background-color: #FFAF03;
+            border-radius: 6px;
+        },
+        ::-webkit-scrollbar-corner {
+          display: none;
+        }
+        `,
+    },
+  },
 })
